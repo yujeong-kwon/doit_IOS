2293576b9cb2035120483d14a1cedde44659e0db
Kyujeong <kyuj1102@gmail.com>
2022-05-23 02:40:57 +0900

    0522_chap11

---
+++ Navigation/Navigation/EditViewController.swift
@@ -0,0 +1,32 @@
+//
+//  EditViewController.swift
+//  Navigation
+//
+//  Created by 권유정 on 2022/05/23.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+
+    @IBOutlet var lblWay: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func btnDone(_ sender: UIButton) {
+    }
+}
---
+++ Navigation/Navigation/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by 권유정 on 2022/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+}
+
