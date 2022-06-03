892a1f474736f1a70ee16e19164fb190253c5abd
Kyujeong <kyuj1102@gmail.com>
2022-06-03 22:31:08 +0900

    0603_chap12

---
+++ Table/Table/AddViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AddViewController.swift
+//  Table
+//
+//  Created by 권유정 on 2022/06/03.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    @IBOutlet var tfAddItem: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func btnAddItem(_ sender: UIButton) {
+        items.append(tfAddItem.text!)
+        itemsImageFile.append("clock.png")
+        tfAddItem.text=""
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+}
