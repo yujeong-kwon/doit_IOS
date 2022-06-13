8d6a808a9c2228cb68b1cb38beea887d6aa6252d
Kyujeong <kyuj1102@gmail.com>
2022-06-13 18:40:50 +0900

    0613_chap16Mission

--- DrawGraphics/DrawGraphics/ViewController.swift
+++ DrawGraphics/DrawGraphics/ViewController.swift
@@ -134,5 +134,50 @@ class ViewController: UIViewController {
         UIGraphicsEndImageContext()
         
     }
+    @IBAction func btnDrawFlower(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.green.cgColor)
+        context.setFillColor(UIColor.green.cgColor)
+        
+        context.move(to: CGPoint(x: 170, y: 200))
+        context.addLine(to: CGPoint(x: 145, y: 400))
+        context.addLine(to: CGPoint(x: 195, y: 400))
+        context.addLine(to: CGPoint(x: 170, y: 200))
+        context.fillPath()
+        context.strokePath()
+        
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.addEllipse(in: CGRect(x: 120, y: 100, width: 100, height: 100))
+        context.strokePath()
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.addEllipse(in: CGRect(x: 70, y: 150, width: 100, height: 100))
+        context.strokePath()
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.addEllipse(in: CGRect(x: 170, y: 150, width: 100, height: 100))
+        context.strokePath()
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.addEllipse(in: CGRect(x: 120, y: 200, width: 100, height: 100))
+        context.strokePath()
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.addEllipse(in: CGRect(x: 120, y: 150, width: 100, height: 100))
+        context.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+    }
 }
 
