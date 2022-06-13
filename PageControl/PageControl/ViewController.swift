b52cf75556292aeaaf365538bca1ac4872ff2741
Kyujeong <kyuj1102@gmail.com>
2022-06-13 23:56:40 +0900

    chap18Mission

--- PageControl/PageControl/ViewController.swift
+++ PageControl/PageControl/ViewController.swift
@@ -20,11 +20,36 @@ class ViewController: UIViewController {
         pageControl.pageIndicatorTintColor = UIColor.green
         pageControl.currentPageIndicatorTintColor = UIColor.red
         imgView.image = UIImage(named: images[0])
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
+        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        self.view.addGestureRecognizer(swipeRight)
     }
 
     @IBAction func pageChange(_ sender: UIPageControl) {
         imgView.image = UIImage(named: images[pageControl.currentPage])
     }
     
+    @objc func respondToSwipeGesture(_ gesture:  UIGestureRecognizer){
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer{
+            switch swipeGesture.direction{
+            case UISwipeGestureRecognizer.Direction.left:
+                if(pageControl.currentPage < pageControl.numberOfPages-1){
+                    pageControl.currentPage = pageControl.currentPage + 1
+                }
+            case UISwipeGestureRecognizer.Direction.right:
+                if(pageControl.currentPage > 0){
+                    pageControl.currentPage = pageControl.currentPage - 1
+                }
+            default:
+                break
+            }
+            imgView.image = UIImage(named: images[pageControl.currentPage])
+        }
+    }
 }
 
